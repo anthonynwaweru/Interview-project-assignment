700612899c71aab4b825a04926e0c9560ab2e6dd
anthonynwaweru <anthonypmarcel@gmail.com>
2022-11-08 11:52:28 +0300

    bash script update

--- stack-setup.sh
+++ stack-setup.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#! /bin/sh
 #make sure you have created your SSH keys in home directory
 #enter your server public ip Addre
 echo 'enter ip address' 
@@ -7,15 +7,47 @@ read publicIp
 # read keyFile
 echo "enter user"
 read user
-ssh $user@$publicIp << EOF "
-sudo -s;
-sudo apt update;
+
+ 
+ssh $user@$publicIp "
+
+sudo apt-get update;
 sudo apt-get upgrade;
-sudo apt install docker;
-git clone https://github.com/discourse/discourse_docker.git /var/discourse;
+sudo apt-get install ca-certificates curl gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+mkdir /var/discourse
+sudo git clone https://github.com/discourse/discourse_docker.git /var/discourse;
 cd /var/discourse;
-./discourse-setup;
-cd containers/app.yml;
-ls;
+sudo ./discourse-setup;
 "
-EOF
+
+
+# when this :[<<"containers/app.yml memory parameters updated" >>] is diplayed on the terminal 
+#Enter your personal details as shown below
+
+#prompt 1, Hostname for your Discourse? [discourse.example.com]: 
+    #ENTER
+
+#prompt 2 Email address for admin account(s)? [me@example.com,you@example.com]:     
+    #ENTER
+
+#prompt 3 SMTP server address? [smtp.example.com]: 
+    #Enter
+
+#prompt 4 SMTP port? [587]: 
+    #ENTER
+
+#prompt 5 SMTP user name? [user@example.com]: 
+    #ENTER
+
+#prompt 6 SMTP password? [pa$$word]: 
+    #ENTER
+
+#prompt 7 Let's Encrypt account email? (ENTER to skip) [me@example.com]: 
+    #ENTER
+
+# Optional Maxmind License key () [xxxxxxxxxxxxxxxx]:
+    #ENTER
